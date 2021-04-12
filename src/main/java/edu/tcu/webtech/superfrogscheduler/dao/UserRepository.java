2b2987cada44791b460f25103af31616eb0115d9
rluly <ryanluly1@gmail.com>
2021-04-12 09:04:36 -0700

    Update UserRepository.java

--- src/main/java/edu/tcu/webtech/superfrogscheduler/dao/UserRepository.java
+++ src/main/java/edu/tcu/webtech/superfrogscheduler/dao/UserRepository.java
@@ -1,4 +1,8 @@
 package edu.tcu.webtech.superfrogscheduler.dao;
 
-public class UserRepository {
+import edu.tcu.webtech.superfrogscheduler.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository extends JpaRepository<User,Integer>{
+    public User findByUsername(String username);
 }
